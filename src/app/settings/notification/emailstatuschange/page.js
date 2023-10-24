54add0896268ec6b9e87303eb1d128110e7a21d0
rajesh-itgeeks <rajeshchoudhary@itgeeks.com>
2023-10-24 13:23:44 +0530

    Notification

--- src/app/settings/notification/emailstatuschange/page.js
+++ src/app/settings/notification/emailstatuschange/page.js
@@ -1,6 +1,5 @@
 import Image from 'next/image'
-import EmailStatusChangePage from '@/Components/Settings/EmailStatusChangePage'
 
 export default function EmailStatusChange() {
-  return <EmailStatusChangePage />;
+  return <>fgfdg</>;
 }
